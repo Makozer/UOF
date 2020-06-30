3f808478986ac367b411df8d82f7728fdc180f3d
Makozer <mk113@web.de>
2020-06-30 19:29:23 +0200

    Documentation Part 2

--- src/community/message/CommunityMessage.java
+++ src/community/message/CommunityMessage.java
@@ -2,8 +2,21 @@ package community.message;
 
 import java.util.Date;
 
+/**
+ * Used for all communication between users
+ * @author Martin
+ *
+ */
 public class CommunityMessage extends Message {
 
+	/**
+	 * From a PlayerId to a PlayerId with a title and a message and a timestamp :)
+	 * @param fromId
+	 * @param toId
+	 * @param title
+	 * @param message
+	 * @param timestamp
+	 */
 	public CommunityMessage(int fromId, int toId, String title, String message, Date timestamp) {
 		super(fromId, toId, title, message, timestamp);
 	}
